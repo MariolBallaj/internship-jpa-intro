9585b48c9fe91ec3ce6165187bf01ff8396aa5a4
Mariol Ballaj <mariol.ballaj@lhind.dlh.de>
2022-06-16 14:34:31 +0200

    JPA Introduction project setup and examples

---
+++ src/main/java/util/Queries.java
@@ -0,0 +1,13 @@
+package util;
+
+public final class Queries {
+
+    private Queries() {}
+
+    public static final String FIND_ALL_EMPLOYEES = "SELECT e FROM Employee e";
+
+    public static final String FIND_EMPLOYEE_BY_FIRST_NAME_METHOD_1 = "SELECT e FROM Employee e WHERE e.firstName = ?1";
+
+    public static final String FIND_EMPLOYEE_BY_FIRST_NAME_METHOD_2 = "SELECT e FROM Employee e WHERE e.firstName = :firstName";
+
+}
